452851644850dea8a3006d4094f906c9672564b8
Fred Mandrea <36716284+actifred@users.noreply.github.com>
2019-03-21 11:50:13 +0100

    Add files via upload

---
+++ generateur.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+    var faker = require('faker');
+    var _ = require('lodash');
+    return {
+        users: _.times(50, (n) => {
+            return {
+                id: n,
+                firstname: faker.name.firstName(),
+                lastname: faker.name.lastName(),
+                avatar: faker.internet.avatar()
+            }
+        })
+    }
+}
